6a17866572afaae8a1593868651104d2460a373a
Derek Miller <derek.miller@arm.com>
2020-11-12 21:49:32 +0000

    Moved send_buffer and receive_buffer into veracruz-utils to reduce duplication

--- mexico-city/src/mc_nitro.rs
+++ mexico-city/src/mc_nitro.rs
@@ -14,7 +14,7 @@ use nix::sys::socket::listen as listen_vsock;
 use nix::sys::socket::{accept, bind, recv, send, MsgFlags, SockAddr};
 use nix::sys::socket::{socket, AddressFamily, SockFlag, SockType};
 use std::os::unix::io::RawFd;
-use veracruz_utils::{MCMessage, NitroStatus};
+use veracruz_utils::{MCMessage, NitroStatus, receive_buffer, send_buffer};
 
 const CID: u32 = 0xFFFFFFFF; // VMADDR_CID_ANY
 const PORT: u32 = 5006;
@@ -77,103 +77,4 @@ pub fn nitro_main() -> Result<(), String> {
         send_buffer(fd, &return_buffer)
             .map_err(|err| format!("mc_nitro::main send_buffer failed:{:?}", err))?;
     }
-}
-
-fn receive_buffer(fd: RawFd) -> Result<Vec<u8>, String> {
-    // first, read the length
-    println!("Chiapas::receive_buffer started with fd:{:?}", fd);
-    let length = {
-        let mut buf = [0u8; 9];
-        let len = buf.len();
-        let mut received_bytes = 0;
-        println!("iterating until we receive len:{:?}", len);
-        while received_bytes < len {
-            received_bytes += match recv(fd, &mut buf[received_bytes..len], MsgFlags::empty()) {
-                Ok(size) => {
-                    size
-                },
-                Err(nix::Error::Sys(EINTR)) => 0,
-                Err(err) => {
-                    println!("I have experienced an error");
-                    return Err(format!(
-                        "SinaloaNitro::receive_buffer failed to read bytes of length:{:?}",
-                        err
-                    ))
-                }
-            }
-        }
-        println!("Received num bytes:{:?}", received_bytes);
-        println!("Received buffer:{:?}", buf);
-        println!("Attempting little endian conversion");
-        LittleEndian::read_u64(&buf) as usize
-    };
-    println!("CHiapas::receive_buffer has read length:{:?}", length);
-    let mut buffer: Vec<u8> = vec![0; length];
-    // next, read the buffer
-    {
-        let mut received_bytes: usize = 0;
-        while received_bytes < length {
-            received_bytes += match recv(fd, &mut buffer[received_bytes..length], MsgFlags::empty())
-            {
-                Ok(size) => size,
-                Err(nix::Error::Sys(EINTR)) => 0,
-                Err(err) => {
-                    return Err(format!(
-                        "SinaloaNitro::receive_buffer failed to read bytes to buffer:{:?}",
-                        err
-                    ))
-                }
-            }
-        }
-    }
-    println!("Chiapas::receive_buffer has finished:{:?}", buffer);
-    return Ok(buffer);
-}
-
-pub fn send_buffer(fd: RawFd, buffer: &Vec<u8>) -> Result<(), String> {
-    println!("Chiapas::send_buffer started with fd:{:?}", fd);
-    let len = buffer.len();
-    // first, send the length of the buffer
-    {
-        let mut buf = [0u8; 9];
-        LittleEndian::write_u64(&mut buf, buffer.len() as u64);
-        let mut sent_bytes = 0;
-        println!("Chiapas::send_buffer sending this number of bytes:{:?}", buf.len());
-        while sent_bytes < buf.len() {
-            sent_bytes += match send(fd, &buf[sent_bytes..buf.len()], MsgFlags::empty()) {
-                Ok(size) => size,
-                //Err(nix::Error::Sys(EINTR)) =>{
-                    //println!("Chiapas::send_buffer as encountered EINTR error");
-                    //0
-                //},
-                Err(err) => {
-                    return Err(format!(
-                        "SinaloaNitro::send_buffer failed to send bytes of length:{:?}",
-                        err
-                    ))
-                }
-            };
-            println!("Chiapas::send_buffer has send this number of bytes so far:{:?}",sent_bytes);
-        }
-    }
-    println!("Chiapas::send_buffer has sent the length:{:?}", len);
-    // next, send the buffer
-    {
-        let mut sent_bytes = 0;
-        while sent_bytes < len {
-            let size = match send(fd, &buffer[sent_bytes..len], MsgFlags::empty()) {
-                Ok(size) => size,
-                Err(nix::Error::Sys(_)) => 0,
-                Err(err) => {
-                    return Err(format!(
-                        "SinaloaNitro: send_buffer failed to send bytes:{:?}",
-                        err
-                    ))
-                }
-            };
-            sent_bytes += size;
-        }
-    }
-    println!("Chiapas::send_buffer has completed.");
-    return Ok(());
 }
