a7cf2d3ac533163607ffd14e497dc2319858520f
Shale Xiong <shale.xiong@arm.com>
2020-11-13 16:05:40 +0000

    Split the sinaloa-test into stage because streaming examples comsume massive memory.

--- run_sinaloa_test_tz.sh
+++ run_sinaloa_test_tz.sh
@@ -7,9 +7,9 @@
 #
 # See the `LICENSE.markdown` file in the Veracruz root directory for licensing
 # and copyright information.
-./run_tz_test.sh sinaloa_test 60
 
 # Run test individual phases:
-#./run_tz_test.sh "sinaloa_test test_phase1"  20
-#./run_tz_test.sh "sinaloa_test test_phase2"  20
-#./run_tz_test.sh "sinaloa_test test_phase3"  20
+./run_tz_test.sh "sinaloa_test test_phase1"  20
+./run_tz_test.sh "sinaloa_test test_phase2"  20
+./run_tz_test.sh "sinaloa_test test_phase3"  20
+./run_tz_test.sh "sinaloa_test test_phase4"  20
