f6c5554af35f42c3486400f514828e790cd65982
Dominic Mulligan <dominic.p.mulligan@gmail.com>
2020-12-02 11:13:25 +0000

    wasi-abi: fixed broken return codes for unsupported functions
    Also, removed material from `fs` module in favour of a complete rewrite.

---
+++ execution-engine/src/hcall/wasi/fs.rs
@@ -0,0 +1,14 @@
+//! A synthetic filesystem.
+//!
+//! This file defines a simple filesystem with named directories and files.
+//!
+//! ## Authors
+//!
+//! The Veracruz Development Team.
+//!
+//! ## Licensing and copyright notice
+//!
+//! See the `LICENSE.markdown` file in the Veracruz root directory for
+//! information on licensing and copyright.
+
+pub(crate) type FD = u32;
