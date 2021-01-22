a9f5fd1f3a0efd69e86d422dddd797ad949c921a
Derek D. Miller <derek.miller@arm.com>
2021-01-22 18:43:08 +0000

    Added RustDoc comments to vsocket.rs

--- veracruz-utils/src/vsocket.rs
+++ veracruz-utils/src/vsocket.rs
@@ -14,20 +14,24 @@ use nix::sys::socket::{AddressFamily, Shutdown, SockAddr, SockFlag, SockType};
 use nix::unistd::close;
 use std::os::unix::io::{AsRawFd, RawFd};
 
-// Maximum number of connection attempts
+/// Maximum number of connection attempts to make before erroring out
 const MAX_CONNECTION_ATTEMPTS: usize = 5;
 
+/// A struct containing the details of a VSOCK
 pub struct VsockSocket {
     socket_fd: RawFd,
 }
 
 impl VsockSocket {
+    /// create a new VsockSocket from a RawFd for a socket
     fn new(socket_fd: RawFd) -> Self {
         VsockSocket { socket_fd }
     }
 }
 
 impl Drop for VsockSocket {
+    /// Drop a socket by closing it. If it fails, report a message, but don't
+    /// do anything else (it could fail because it's not connected)
     fn drop(&mut self) {
         shutdown(self.socket_fd, Shutdown::Both)
             .unwrap_or_else(|_| ()); // Do nothing on failure. It's non fatal and a warning message is just confusing
@@ -36,6 +40,7 @@ impl Drop for VsockSocket {
 }
 
 impl AsRawFd for VsockSocket {
+    /// Extract the raw RadFd from the VsockSocket
     fn as_raw_fd(&self) -> RawFd {
         self.socket_fd
     }
