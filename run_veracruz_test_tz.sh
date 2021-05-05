08cd6b1951cf26b1efafdb3c0517179b31213f28
Shale Xiong <shale.xiong@arm.com>
2021-05-05 09:37:38 +0100

    Increate the waiting time in veracruz test on tz.

--- run_veracruz_test_tz.sh
+++ run_veracruz_test_tz.sh
@@ -7,7 +7,7 @@
 #
 # See the `LICENSE.markdown` file in the Veracruz root directory for licensing
 # and copyright information.
-./run_tz_test.sh veracruz_test 200
+./run_tz_test.sh veracruz_test 400
 
 # Run test individual phases:
 #./run_tz_test.sh "veracruz_test veracruz_phase1"  100
