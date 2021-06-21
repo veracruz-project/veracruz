97bad812d2ebf568dcd9cdc71b0e74f170daf3ad
Dominic Mulligan <dominic.mulligan@arm.com>
2021-06-21 14:54:21 +0100

    end-to-end-demo: renamed test-program, better description of what is happening in demo
    ...updated policy file to reflect changes.

--- end-to-end-demo/src/main.rs
+++ end-to-end-demo/src/main.rs
@@ -37,7 +37,6 @@
 use actix_rt::System;
 use anyhow::Result;
 use env_logger;
-use log::{error, info};
 use rand::seq::SliceRandom;
 use ring::digest::{digest, SHA256};
 use serde::{Deserialize, Serialize};
@@ -65,7 +64,8 @@ use veracruz_utils::{platform::Platform, policy::policy::Policy};
 
 /// The path of the WASM routing program supplied by the mapping service that
 /// will be used for the collaborative computation.
-const WASM_BINARY_PATH: &'static str = "test-program/target/wasm32-wasi/release/test-program.wasm";
+const WASM_BINARY_PATH: &'static str =
+    "oblivious-routing/target/wasm32-wasi/release/oblivious-routing.wasm";
 /// The filename of the WASM binary when stored in Veracruz's Virtual File
 /// System (VFS).
 const WASM_BINARY_VFS_PATH: &'static str = "test-program.wasm";
@@ -348,11 +348,6 @@ fn generate_challenge() -> Challenge {
         .expect("Random location could not be chosen (locations list may be empty).")
         .clone();
 
-    info!(
-        "Chosen locations {} to {}.",
-        first_location, second_location
-    );
-
     Challenge::new(first_location, second_location)
 }
 
@@ -360,15 +355,20 @@ fn generate_challenge() -> Challenge {
 // Waiting to proceed.
 ////////////////////////////////////////////////////////////////////////////////
 
-/// Prints a prompt to `stdout` asking for the user to provide input, then
-/// blocks waiting for input.  This is just a simple utility function to help
-/// "show off" the different stages of the Veracruz provisioning flow.
-fn wait_for_user() {
+/// Prints a prompt to `stdout` describing the next step and thereafter asking for
+/// the user to provide input, then blocks waiting for input.  This is just a simple
+/// utility function to help "show off" the different stages of the Veracruz
+/// provisioning flow.
+fn wait_for_user<T>(msg: T)
+where
+    T: AsRef<str>,
+{
+    println!("\nNext step: {}\n", msg.as_ref());
     println!(">>> Press any key to continue...");
 
     let mut line = String::new();
     stdin().read_line(&mut line).unwrap_or_else(|e| {
-        error!("Failed to read from stdin.  Error produced: {}.", e);
+        eprintln!("Failed to read from stdin.  Error produced: {}.", e);
         exit(1)
     });
 }
@@ -381,19 +381,20 @@ fn main() -> anyhow::Result<()> {
     /* Setup logging to make debugging any errors easier. */
 
     env_logger::init();
+
     std::env::set_var("RUST_LOG", RUST_LOG_SETTINGS);
 
-    info!(
-        "Logging initialized (with settings: {}).",
-        RUST_LOG_SETTINGS
+    println!(
+         "Logging initialized (with settings: {}).\nNote: run end-to-end-demo with `RUST_LOG=info,error` set to debug runtime failures in Veracruz code.",
+         RUST_LOG_SETTINGS
     );
 
-    wait_for_user();
+    wait_for_user(format!("Reading policy file ({}).", POLICY_PATH));
 
     /* Read the policy file. */
 
     let mut policy_file = File::open(&POLICY_PATH).map_err(|e| {
-        error!(
+        eprintln!(
             "Failed to open policy file ({}).  Error produced: {}.",
             POLICY_PATH, e
         );
@@ -404,7 +405,7 @@ fn main() -> anyhow::Result<()> {
     policy_file
         .read_to_string(&mut policy_content)
         .map_err(|e| {
-            error!(
+            eprintln!(
                 "Failed to read the content of the policy file ({}).  Error produced: {}.",
                 POLICY_PATH, e
             );
@@ -412,25 +413,22 @@ fn main() -> anyhow::Result<()> {
         })?;
 
     let policy = Policy::from_json(&policy_content).map_err(|e| {
-        error!(
+        eprintln!(
             "Failed to parse JSON policy file ({}).  Error produced: {:?}.",
             POLICY_PATH, e
         );
         e
     })?;
 
-    info!("Policy file read ({}).", POLICY_PATH);
-
-    wait_for_user();
-
-    /* Compute a hash of the content of the policy file. */
-
     let policy_hash = digest(&SHA256, policy_content.as_bytes());
     let hex_policy_hash = hex::encode(&policy_hash.as_ref().to_vec());
 
-    info!("Policy file has SHA-256 hash: {}.", hex_policy_hash);
+    println!(
+        "Policy file read, and has SHA-256 hash: {}.",
+        hex_policy_hash
+    );
 
-    wait_for_user();
+    wait_for_user("Bringing up the Veracruz Proxy Attestation Server, which abstracts over the various attestation protocols in use within Veracruz.");
 
     /* Start the Veracruz proxy attestation server. */
 
@@ -443,7 +441,7 @@ fn main() -> anyhow::Result<()> {
             proxy_attestation_server::server::server(proxy_attestation_server_url, false).unwrap();
 
         let _result = sys.block_on(server).map_err(|e| {
-            error!(
+            eprintln!(
                 "Failed to initialize Veracruz Proxy Attestation Server.  Error produced: {}.",
                 e
             );
@@ -451,14 +449,16 @@ fn main() -> anyhow::Result<()> {
         });
     });
 
-    sleep(Duration::from_secs(8));
+    println!("Veracruz Proxy Attestation Server initializing.  Waiting 10 seconds to complete...");
 
-    info!(
-        "Veracruz Proxy Attestation Server now initialized (at {}).",
+    sleep(Duration::from_secs(10));
+
+    println!(
+        "Veracruz Proxy Attestation Server now initialized and listening at {}.",
         policy.proxy_attestation_server_url()
     );
 
-    wait_for_user();
+    wait_for_user("Bringing up the Veracruz Server, which acts as a bridge between untrusted and trusted code.");
 
     /* Bring up the Veracruz server. */
 
@@ -468,7 +468,7 @@ fn main() -> anyhow::Result<()> {
         let server = veracruz_server::server::server(&POLICY_PATH).unwrap();
 
         let _result = sys.block_on(server).map_err(|e| {
-            error!(
+            eprintln!(
                 "Failed to initialize Veracruz Server.  Error produced: {}.",
                 e
             );
@@ -476,11 +476,13 @@ fn main() -> anyhow::Result<()> {
         });
     });
 
-    sleep(Duration::from_secs(8));
+    println!("Veracruz Server initializing.  Waiting 10 seconds to complete...");
 
-    info!("Veracruz Server now initialized.");
+    sleep(Duration::from_secs(10));
 
-    wait_for_user();
+    println!("Veracruz Server now initialized, and trusted Veracruz runtime code attested against expected hash in the policy.");
+
+    wait_for_user("Creating two clients: one for the mapping service provider through which they provision the routing program and the underlying graph data, and the other for the mapping service user, through which they provision the routing challenge and retrieve the computed route.");
 
     /* Describe the two clients that will be connecting to the server in the
      * computation.
@@ -493,7 +495,7 @@ fn main() -> anyhow::Result<()> {
         &Platform::SGX,
     )
     .map_err(|e| {
-        error!(
+        eprintln!(
             "Failed to describe mapping service client.  Error produced: {:?}.",
             e
         );
@@ -507,21 +509,23 @@ fn main() -> anyhow::Result<()> {
         &Platform::SGX,
     )
     .map_err(|e| {
-        error!(
+        eprintln!(
             "Failed to describe mapping user client.  Error produced: {:?}.",
             e
         );
         e
     })?;
 
-    info!("Mapping service and user clients created.");
+    println!("Mapping service and user clients created, and certificates checked against the policy file.");
 
-    wait_for_user();
+    wait_for_user(
+        "WASM binary opened and provisioned into trusted Veracruz runtime via secure TLS link.",
+    );
 
     /* Read the WASM program in preparation of provisioning. */
 
     let mut wasm_binary_file = File::open(&WASM_BINARY_PATH).map_err(|e| {
-        error!(
+        eprintln!(
             "Failed to open WASM binary file ({}).  Error produced: {}.",
             WASM_BINARY_PATH, e
         );
@@ -532,33 +536,34 @@ fn main() -> anyhow::Result<()> {
     wasm_binary_file
         .read_to_end(&mut wasm_binary_content)
         .map_err(|e| {
-            error!(
+            eprintln!(
                 "Failed to read content of WASM binary file ({}).  Error produced: {}.",
                 WASM_BINARY_PATH, e
             );
             e
         })?;
 
-    info!("WASM binary ({}) read successfully.", WASM_BINARY_PATH);
-
-    wait_for_user();
-
-    /* Compute the hash of the WASM binary. */
-
     let wasm_binary_hash = digest(&SHA256, &wasm_binary_content);
     let hex_wasm_binary_hash = hex::encode(&wasm_binary_hash.as_ref().to_vec());
 
-    info!("WASM binary has SHA-256 hash: {}.", hex_wasm_binary_hash);
+    println!(
+        "WASM binary read successfull and has SHA-256 hash: {}.",
+        hex_wasm_binary_hash
+    );
 
-    wait_for_user();
+    wait_for_user("Obtain the routing map.");
 
     /* Generate and serialize the routing map. */
 
     let graph = generate_graph();
     let serialized_graph = to_vec(&graph).map_err(|e| {
-        error!("Failed to serialize routing map.  Error produced: {}.", e);
+        eprintln!("Failed to serialize routing map.  Error produced: {}.", e);
         e
     })?;
+
+    println!("Graph read (nodes: {}).", graph.nodes.len());
+
+    wait_for_user("The Mapping Service Client provisions the routing binary and graph securely into the Veracruz runtime, via TLS.  These will be both stored in the Veracruz runtime's Virtual File System (VFS) at paths specified in the policy file.");
 
     /* Provision the program, via the program provider client.  Note that this
      * implicitly checks that the policy in force is the one that is expected.
@@ -567,7 +572,7 @@ fn main() -> anyhow::Result<()> {
     mapping_service_client
         .send_program(&WASM_BINARY_VFS_PATH, &wasm_binary_content)
         .map_err(|e| {
-            error!(
+            eprintln!(
                 "Failed to provision WASM program ({}).  Error produced: {:?}.",
                 WASM_BINARY_PATH, e
             );
@@ -577,42 +582,42 @@ fn main() -> anyhow::Result<()> {
     mapping_service_client
         .send_data(&ROUTING_GRAPH_VFS_PATH, &serialized_graph)
         .map_err(|e| {
-            error!("Failed to provision routing map.  Error produced: {:?}.", e);
+            eprintln!("Failed to provision routing map.  Error produced: {:?}.", e);
             e
         })?;
 
     mapping_service_client.request_shutdown().map_err(|e| {
-        error!(
+        eprintln!(
             "Failed to shutdown program provider client.  Error produced: {:?}.",
             e
         );
         e
     })?;
 
-    info!(
-        "WASM program ({}) and routing map provisioned successfully.  Now stored in Veracruz VFS (at {} and {}).",
+    println!(
+        "WASM program ({}) and routing map provisioned successfully.  Now stored in Veracruz VFS at {} and {}.",
         WASM_BINARY_PATH, WASM_BINARY_VFS_PATH, ROUTING_GRAPH_VFS_PATH
     );
 
-    wait_for_user();
+    wait_for_user("Generate a routing challenge.");
 
     /* Generate and serialize a routing challenge. */
 
     let challenge = generate_challenge();
     let serialized_challenge = to_vec(&challenge).map_err(|e| {
-        error!(
+        eprintln!(
             "Failed to serialize routing challenge.  Error produced: {:?}.",
             e
         );
         e
     })?;
 
-    info!(
-        "Requesting route from {} to {}.",
+    println!(
+        "Routing challenge successfully generated: requesting a route from {} to {}.",
         &challenge.source, &challenge.sink
     );
 
-    wait_for_user();
+    wait_for_user("The Mapping User Client provisions the routing challenge securely into the Veracruz runtime via TLS.  This is again stored in a file in the Veracruz runtime's Virtual File System (VFS) as specified in the policy, ready for reading by the routing program.");
 
     /* Provision the data input, via the data provider client. Note that this
      * also implicitly checks that the policy in force is the one that is
@@ -622,58 +627,63 @@ fn main() -> anyhow::Result<()> {
     mapping_user_client
         .send_data(ROUTING_CHALLENGE_VFS_PATH, &serialized_challenge)
         .map_err(|e| {
-            error!(
+            eprintln!(
                 "Failed to provision routing challenge input.  Error produced: {:?}.",
                 e
             );
             e
         })?;
 
-    info!(
-        "Routing challenge provisioned successfully.  Now stored in Veracruz VFS (at {}).",
+    println!(
+        "Routing challenge provisioned successfully.  Now stored in Veracruz VFS at {}.",
         ROUTING_CHALLENGE_VFS_PATH
     );
 
-    wait_for_user();
+    wait_for_user("Everything is now in place, and the Mapping User Client can request the result of the computation.");
 
     /* Now, everything is in place to request the routing result. */
 
     let result = mapping_user_client
         .get_results(&WASM_BINARY_VFS_PATH)
         .map_err(|e| {
-            error!(
+            eprintln!(
                 "Failed to retrieve result of computation.  Error produced: {:?}.",
                 e
             );
             e
         })?;
 
-    info!("Received {} bytes of result.", result.len());
+    println!(
+        "Computation successfully completed, received {} bytes of result.",
+        result.len()
+    );
 
-    wait_for_user();
+    wait_for_user("We now decode the result into something more intelligible.");
 
     /* Now, decode the raw result into something more intelligible. */
 
     let result: Response = from_bytes(&result).map_err(|e| {
-        error!("Failed to decode routing response.  Error produced: {}.", e);
+        eprintln!("Failed to decode routing response.  Error produced: {}.", e);
         e
     })?;
 
-    info!("Decoded result: {}.", result);
+    println!("Computed route: {}", result);
 
-    wait_for_user();
+    wait_for_user(
+        "We request a shutdown of the various Veracruz components, via the Mapping User Client.",
+    );
 
     /* Shutdown the data provider client gracefully. */
 
     mapping_user_client.request_shutdown().map_err(|e| {
-        error!(
+        eprintln!(
             "Failed to shutdown data input provider client.  Error produced: {:?}.",
             e
         );
         e
     })?;
 
-    info!("All done...");
+    println!("All done...");
 
     Ok(())
 }
