f7bf8fda198c1e145019a81e4d8f17d6f720c66c
Christopher Haster <chaster@utexas.edu>
2021-07-14 19:16:30 -0500

    Added ./run_cli_test.sh and attempted to add to CI
    Pushing this up to test

---
+++ run_cli_test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# AUTHORS
+#
+# The Veracruz Development Team.
+#
+# COPYRIGHT
+#
+# See the `LICENSE.markdown` file in the Veracruz root directory for licensing
+# and copyright information.
+#
+
+# Runs the example in GETTING_STARTED_CLI.markdown as a bash script, with any
+# failing commands results in an error
+#
+
+set -euo pipefail
+
+# start from scratch
+rm GETTING_STARTED_CLI.markdown.sh
+
+# make it so any error results in script failure
+echo 'set -euxo pipefail' >> GETTING_STARTED_CLI.markdown.sh
+
+# grab every bash code block, remove line continuation, and only keep lines
+# that start with '$' (of course removing that '$' in the process)
+sed -n '/``` bash/,/```/{/```/d; p}' GETTING_STARTED_CLI.markdown \
+    | sed ':a; /\\$/{N; s/\\\n//; ta}' \
+    | sed -n '/^\$/{s/^\$ \?//; p}' \
+    >> GETTING_STARTED_CLI.markdown.sh
+
+# run script
+bash GETTING_STARTED_CLI.markdown.sh
