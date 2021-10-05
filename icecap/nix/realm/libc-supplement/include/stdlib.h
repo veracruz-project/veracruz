a40d2259730c884b6790f7dc6afe45a743eafe68
Nick Spinale <nick@nickspinale.com>
2021-10-05 16:59:08 +0000

    icecap: Add authorship, licensing, and copyright information to C files

--- icecap/nix/realm/libc-supplement/include/stdlib.h
+++ icecap/nix/realm/libc-supplement/include/stdlib.h
@@ -1,3 +1,15 @@
+/*
+ * AUTHORS
+ *
+ * The Veracruz Development Team.
+ *
+ * COPYRIGHT
+ *
+ * See the `LICENSE_MIT.markdown` file in the Veracruz root directory for licensing
+ * and copyright information.
+ *
+ */
+
 #pragma once
 
 #include <stddef.h>
