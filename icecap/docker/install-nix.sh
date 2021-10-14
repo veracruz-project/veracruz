1a464539492042d050c56656f26a4f7ae23b2c90
Nick Spinale <nick@nickspinale.com>
2021-10-14 02:36:38 +0000

    icecap: docker: Bump nix installer

--- icecap/docker/install-nix.sh
+++ icecap/docker/install-nix.sh
@@ -12,8 +12,8 @@
 set -e
 
 script_name="install-min-nix.sh"
-script_url="https://github.com/nspin/minimally-invasive-nix-installer/raw/dist-62j2x1q9zy/dist/install-min-nix.sh"
-script_sha256="6882ea0e9b9a12750028d589a17e9e7c184dc0058eb87be3a4064a5c53bfc73e"
+script_url="https://github.com/nspin/minimally-invasive-nix-installer/raw/dist-y64jgzkzg5/dist/install-min-nix.sh"
+script_sha256="f6772ec8ed9b4cb2253167b3d5e6091e9ddc365f170c3c043a39c8f3350ea291"
 
 curl -fL "$script_url" -o "$script_name"
 echo "$script_sha256 $script_name" | sha256sum -c -
