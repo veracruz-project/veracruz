d27ad1744b1fc7505b49fc63c8820e6ae876fcde
Nick Spinale <nick@nickspinale.com>
2021-10-15 15:11:22 +0000

    platform-services: icecap: Drop placeholder RNG
    new_fd and new_inode no longer depend on RNG.

--- platform-services/src/icecap_platform_services.rs
+++ platform-services/src/icecap_platform_services.rs
@@ -14,24 +14,24 @@ use core::sync::atomic::{AtomicU64, Ordering};
 
 static RNG_STATE: AtomicU64 = AtomicU64::new(0);
 
-/// Fill `buffer` with random bytes.
+/// Fills a buffer, `buffer`, with random bytes sampled from the thread-local
+/// random number source.
 ///
-/// Until IceCap provides randomness for realms, this is just a placeholder
-// generator with a fixed seed and a period of 2**61.
+/// Not yet implemented in IceCap.
 pub fn platform_getrandom(buffer: &mut [u8]) -> Result<()> {
-    for b in buffer {
-        let state = RNG_STATE.fetch_add(1, Ordering::SeqCst);
-        *b = state.to_ne_bytes()[(state & 0b111) as usize];
-    }
-    Result::Success(())
+    Result::Unavailable
 }
 
 /// Returns the clock resolution in nanoseconds.
+///
+/// Not yet implemented in IceCap.
 pub fn platform_getclockres(_clock_id: u8) -> Result<u64> {
     Result::Unavailable
 }
 
 /// Returns the clock time in nanoseconds.
+///
+/// Not yet implemented in IceCap.
 pub fn platform_getclocktime(_clock_id: u8) -> Result<u64> {
     Result::Unavailable
 }
