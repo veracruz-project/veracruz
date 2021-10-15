55c81a984c663f7b1135aa258c0a473402347259
Nick Spinale <nick@nickspinale.com>
2021-10-15 16:04:08 +0000

    Revert "platform-services: icecap: Drop placeholder RNG"
    This reverts commit 7433e1549213fdd6c0388efe0d18ba9072f74321.

--- platform-services/src/icecap_platform_services.rs
+++ platform-services/src/icecap_platform_services.rs
@@ -14,24 +14,24 @@ use core::sync::atomic::{AtomicU64, Ordering};
 
 static RNG_STATE: AtomicU64 = AtomicU64::new(0);
 
-/// Fills a buffer, `buffer`, with random bytes sampled from the thread-local
-/// random number source.
+/// Fill `buffer` with random bytes.
 ///
-/// Not yet implemented in IceCap.
+/// Until IceCap provides randomness for realms, this is just a placeholder
+// generator with a fixed seed and a period of 2**61.
 pub fn platform_getrandom(buffer: &mut [u8]) -> Result<()> {
-    Result::Unavailable
+    for b in buffer {
+        let state = RNG_STATE.fetch_add(1, Ordering::SeqCst);
+        *b = state.to_ne_bytes()[(state & 0b111) as usize];
+    }
+    Result::Success(())
 }
 
 /// Returns the clock resolution in nanoseconds.
-///
-/// Not yet implemented in IceCap.
 pub fn platform_getclockres(_clock_id: u8) -> Result<u64> {
     Result::Unavailable
 }
 
 /// Returns the clock time in nanoseconds.
-///
-/// Not yet implemented in IceCap.
 pub fn platform_getclocktime(_clock_id: u8) -> Result<u64> {
     Result::Unavailable
 }
