89b542a968fa604e53a5d69808b9fe311fb8440a
Nick Spinale <nick@nickspinale.com>
2021-10-29 19:35:53 +0000

    icecap: Refer to local IceCap source throughout

--- icecap/nix/default.nix
+++ icecap/nix/default.nix
@@ -8,21 +8,7 @@
 # and copyright information.
 
 let
-  icecapRemote = builtins.fetchGit rec {
-    url = "https://gitlab.com/arm-research/security/icecap/icecap-refs.git";
-    ref = "refs/tags/icecap/keep/${builtins.substring 0 32 rev}";
-    rev = "e27e7bf9b4160a718cf59d5e2a97674a8456bdc1";
-    submodules = true;
-  };
-
-  icecapLocal = ../../../icecap;
-
-  # icecapSource = icecapRemote;
-
-  # NOTE to develop using a local checkout of IceCap, replace the above with:
-  icecapSource = icecapLocal;
-
-  icecap = import icecapSource;
+  icecap = import ../icecap;
 
   veracruz = with icecap; lib.flip lib.mapAttrs pkgs.none.icecap.configured (_: configured:
     import ./instance.nix {
