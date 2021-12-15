893519e9490e2e5bc6cc339055b0574cbd270041
Edmund Grimley Evans <edmund.grimley-evans@arm.com>
2021-12-15 10:20:27 +0000

    runtime-manager/src/main.rs: Make feature(rustc_private) conditional.

--- runtime-manager/src/main.rs
+++ runtime-manager/src/main.rs
@@ -11,10 +11,9 @@
 //! See the `LICENSE_MIT.markdown` file in the Veracruz root directory for
 //! information on licensing and copyright.
 
-#![feature(rustc_private)] // added to stop E0658
-
 #![cfg_attr(any(feature = "icecap"), no_main)]
 #![crate_name = "runtime_manager_enclave"]
+#![cfg_attr(any(feature = "icecap"), feature(rustc_private))]
 #![cfg_attr(any(feature = "icecap"), feature(format_args_nl))]
 
 #[cfg(feature = "linux")]
