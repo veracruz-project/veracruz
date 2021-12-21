9c0dc7b49c4342053f2179a2cf1a63b96f218e27
Mathias Brossard <mathias.brossard@arm.com>
2021-12-21 16:38:56 -0600

    Add icecap workspaces

---
+++ workspaces/icecap-runtime/mk/realm.mk
@@ -0,0 +1,83 @@
+# AUTHORS
+#
+# The Veracruz Development Team.
+#
+# COPYRIGHT
+#
+# See the `LICENSE_MIT.markdown` file in the Veracruz root directory for licensing
+# and copyright information.
+
+include ../../icecap/mk/common.mk
+
+rust_target := aarch64-icecap
+
+icecap_c_include_flags := $(foreach x,$(buildInputs),-I$(abspath $(x)/include))
+icecap_c_lib_flags := $(foreach x,$(buildInputs),-L$(abspath $(x)/lib))
+
+icecap_rustflags := \
+	--cfg=icecap_plat=\"$(ICECAP_PLAT)\" \
+	-l static=icecap-utils \
+	-l static=icecap-pure \
+	-l static=c-supplement \
+	$(icecap_c_lib_flags) \
+	--sysroot=$(abspath $(sysroot_dir))
+
+define realm_crate_body
+	RUST_TARGET_PATH=$(abspath icecap/src/rust/support/targets) \
+		cargo tree \
+			--manifest-path Cargo.toml --target $(rust_target) \
+			-p runtime_manager_enclave --features icecap \
+			-v --charset ascii -f "{p} {f}" > $(build_dir)/tree.txt ; \
+	RUST_TARGET_PATH=$(abspath icecap/src/rust/support/targets) \
+	$(call cargo_target_config_prefix,$(rust_target))RUSTFLAGS="$(icecap_rustflags)" \
+	$(call cargo_target_config_prefix,$(rust_target))LINKER="$(LD)" \
+	CC_$(call kebab_to_caml,$(rust_target))="$(CC)" \
+	CC_$(call kebab_to_caml,$(rust_target_build))="$(HOST_CC)" \
+	BINDGEN_EXTRA_CLANG_ARGS="$(icecap_c_include_flags)" \
+		cargo build \
+			-Z unstable-options \
+			--manifest-path Cargo.toml \
+			--target $(rust_target) \
+			-p runtime_manager_enclave --features icecap \
+			--release \
+			-j$$(nproc) \
+			--target-dir $(target_dir) \
+			--out-dir $(bin_dir)
+ endef
+
+.PHONY: runtime-manager
+runtime-manager: sysroot-install
+	$(call realm_crate_body,runtime-manager)
+
+.PHONY: sysroot-install
+sysroot-install: sysroot
+	d=$(sysroot_dir)/lib/rustlib/aarch64-icecap/lib ; \
+	mkdir -p $$d ; \
+	cp -u $(sysroot_target_dir)/aarch64-icecap/release/deps/lib*.rlib $$d ; \
+	d=$(sysroot_dir)/lib/rustlib/x86_64-unknown-linux-gnu/lib/ ; \
+	mkdir -p $$d ; \
+	cp -u $(sysroot_target_dir)/release/deps/*.so $$d
+
+sysroot_rustflags := \
+	--cfg=icecap_plat=\"$(ICECAP_PLAT)\" \
+	$(icecap_c_lib_flags) \
+	-C force-unwind-tables=yes -C embed-bitcode=yes \
+	-Z force-unstable-if-unmarked \
+	--sysroot /dev/null
+
+.PHONY: sysroot
+sysroot:
+	RUST_TARGET_PATH=$(abspath icecap/src/rust/support/targets) \
+	$(call cargo_target_config_prefix,$(rust_target))RUSTFLAGS="$(sysroot_rustflags)" \
+	$(call cargo_target_config_prefix,$(rust_target))LINKER="$(LD)" \
+	BINDGEN_EXTRA_CLANG_ARGS="$(icecap_c_include_flags)" \
+	RUSTC_BOOTSTRAP=1 \
+	__CARGO_DEFAULT_LIB_METADATA="icecap-sysroot" \
+	cargo build \
+		-Z unstable-options \
+		-Z binary-dep-depinfo \
+		--release \
+		--manifest-path sysroot/workspace/Cargo.toml \
+		--target $(rust_target) \
+		-j$$(nproc) \
+		--target-dir $(sysroot_target_dir)
