dbc5aaed26a6a4f584195e6bed70e2d7e57cfdbd
Mathias Brossard <mathias.brossard@arm.com>
2022-02-28 15:27:26 -0600

    Build capdl and elf loaders

--- workspaces/icecap-runtime/mk/virt.mk
+++ workspaces/icecap-runtime/mk/virt.mk
@@ -11,28 +11,25 @@ build: elfloader system-dir
 clean-plat:
 	rm -f $(sel4_dts_path) system
 
+QEMU_BIN = qemu-system-aarch64
+QEMU_BASE_FLAGS = -machine virt,virtualization=on,gic-version=2 \
+		-cpu cortex-a57 -smp 4 -m 3072 \
+		-nographic -semihosting-config enable=on,target=native \
+		-device virtio-serial-device
+QEMU_RUN_FLAGS = -chardev socket,server,host=localhost,port=1234,id=charconsole0 \
+		-device virtconsole,chardev=charconsole0,id=console0 \
+		-serial mon:stdio
+
 .PHONY: run
 run:
 	ln -sf $(system) system
-	qemu-system-aarch64 \
-		-machine virt,virtualization=on,gic-version=2 -cpu cortex-a57 -smp 4 -m 3072 \
-		-nographic \
-		-semihosting-config enable=on,target=native \
-		-device virtio-net-device,netdev=netdev0 \
-		-serial mon:stdio \
-		-netdev user,id=netdev0 \
+	$(QEMU_BIN) $(QEMU_BASE_FLAGS) $(QEMU_RUN_FLAGS) \
 		-kernel $(elfloader)
 
 .PHONY: debug
 debug:
 	ln -sf $(system) system
-	qemu-system-aarch64 \
-		-machine virt,virtualization=on,gic-version=2 -cpu cortex-a57 -smp 4 -m 3072 \
-		-nographic \
-		-semihosting-config enable=on,target=native \
-		-device virtio-net-device,netdev=netdev0 \
-		-serial mon:stdio \
-		-netdev user,id=netdev0 \
+	$(QEMU_BIN) $(QEMU_BASE_FLAGS) $(QEMU_RUN_FLAGS) \
 		-kernel $(elfloader)
 
 sel4-configure: $(sel4_dts_path)
@@ -41,15 +38,8 @@ $(sel4_dts_path): $(misc_build_dir)/virt.dtb
 	dtc -I dtb -O dts -o $@ $<
 
 $(misc_build_dir)/virt.dtb: | $(misc_build_dir)
-	qemu-system-aarch64 \
-		-machine virt,virtualization=on,dumpdtb=$@,gic-version=2 -cpu cortex-a57 -smp 4 -m 3072 \
-		-nographic \
-		-semihosting-config enable=on,target=native \
-		-device virtio-net-device,netdev=netdev0 \
-		-device virtio-9p-device,mount_tag=share,fsdev=share \
-		-netdev user,id=netdev0 \
-		-fsdev local,id=share,security_model=none,readonly,path=.
-
+	$(QEMU_BIN) $(QEMU_BASE_FLAGS) \
+		-machine dumpdtb=$@
 
 system_files := \
 
