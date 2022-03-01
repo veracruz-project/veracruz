422cde1084876a34c76ad1fa688acf92110df7a7
Mathias Brossard <mathias.brossard@arm.com>
2022-03-01 17:23:09 -0600

    Towards consistent Rust build verbosity

--- workspaces/common.mk
+++ workspaces/common.mk
@@ -12,10 +12,19 @@
 PROFILE ?= release
 PROFILE_PATH = release
 PROFILE_FLAG = --release
+V ?= 0
 
-export PROFILE
+export PROFILE V
 
 ifeq ($(PROFILE),dev)
     PROFILE_PATH = debug
     PROFILE_FLAG =
+endif
+
+ifeq ($(V), 0)
+    V_FLAG =
+    Q = @
+else
+    V_FLAG = -v
+    Q =
 endif
