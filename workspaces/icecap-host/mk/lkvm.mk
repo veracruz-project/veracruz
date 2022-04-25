3fe1ea65b6b76026376f1fe47578e304f76be197
Mathias Brossard <mathias.brossard@arm.com>
2022-04-25 15:31:19 -0500

    Link statically icecap-host for lkvm + update IceCap

--- workspaces/icecap-host/mk/lkvm.mk
+++ workspaces/icecap-host/mk/lkvm.mk
@@ -10,7 +10,8 @@
 
 host_feature_flags := --features icecap-lkvm --target aarch64-unknown-linux-gnu
 
-COMPILERS = CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc RUSTFLAGS="-C linker=aarch64-linux-gnu-gcc"
+COMPILERS = CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc \
+	    RUSTFLAGS="-C linker=aarch64-linux-gnu-gcc -C target-feature=+crt-static"
 
 rustup-plat:
 	rustup target add aarch64-unknown-linux-gnu
