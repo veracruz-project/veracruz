69864328038d0b0e892569ee6b25aecb661459c6
Christopher Haster <chaster@utexas.edu>
2022-04-25 23:27:57 -0500

    Enabled explicit build of veracruz-server-test for icecap
    Required for cross-compilation x86 -> aarch64
    
    Eventually we should do this for veracruz-test as well, not sure if it's
    necessary for veracruz-client.

--- workspaces/icecap-host/mk/lkvm.mk
+++ workspaces/icecap-host/mk/lkvm.mk
@@ -13,5 +13,6 @@ host_feature_flags := --features icecap-lkvm --target aarch64-unknown-linux-gnu
 COMPILERS = CC_aarch64_unknown_linux_gnu=aarch64-linux-gnu-gcc \
 	    RUSTFLAGS="-C linker=aarch64-linux-gnu-gcc -C target-feature=+crt-static"
 
+.PHONY: rustup-plat
 rustup-plat:
 	rustup target add aarch64-unknown-linux-gnu
