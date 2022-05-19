e044a3cda5d2537763148fe687bf8a2540779499
Basma El Gaabouri <basma.elgaabouri@arm.com>
2022-05-19 16:47:11 +0100

    use icecap_plat=lkvm_realm (or lkvm) to switch between realm mode and non-realm mode.

---
+++ workspaces/icecap-runtime/mk/lkvm_realm.mk
@@ -0,0 +1,21 @@
+# AUTHORS
+#
+# The Veracruz Development Team.
+#
+# COPYRIGHT
+#
+# See the `LICENSE.markdown` file in the Veracruz root directory for licensing
+# and copyright information.
+
+sel4_kernel_platform := lkvm
+
+sel4_dts_path := $(sel4_src)/tools/dts/lkvm.dts
+
+.PHONY: clean-plat
+clean-plat:
+
+.PHONY: run
+run:
+
+.PHONY: debug
+debug:
