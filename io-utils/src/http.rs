d711780730f435d98fa1846ed7078c1247f4319a
Derek D. Miller <derek.miller@arm.com>
2022-10-27 10:51:42 -0500

    Revamped http code to use reqwest to match upstream changes

--- io-utils/src/http.rs
+++ io-utils/src/http.rs
@@ -13,15 +13,10 @@
 //! and licensing information.
 
 use anyhow::{anyhow, Result as AnyhowResult};
-use curl::{
-    easy::{Easy, List},
-    Error as CurlError,
-};
 use err_derive::Error;
-use log::{error, info, trace};
-use regex::Regex;
-use std::{io::Read, str::from_utf8, string::String, vec::Vec};
-use stringreader::StringReader;
+use log::{error, info};
+use reqwest;
+use std::{string::String, vec::Vec};
 use transport_protocol::{
     ProxyAttestationServerResponse, TransportProtocolError,
 };
@@ -34,12 +29,18 @@ use uuid::Uuid;
 /// HTTP-related errors.
 #[derive(Debug, Error)]
 pub enum HttpError {
-    /// An error originating from Curl was raised.
-    #[error(display = "An error originating from Curl was raised: {}.", _0)]
-    CurlError(CurlError),
+    /// Reqwest generated an error
+    #[error(display = "Reqwest generated an error:{}", _0)]
+    ReqwestError(reqwest::Error),
+    /// Invalid Header value
+    #[error(display = "Invalid header value:{}", _0)]
+    InvalidHeaderValue(reqwest::header::InvalidHeaderValue),
     /// An unexpected HTTP status code was returned.
     #[error(display = "An unexpected HTTP return code was returned.")]
-    HttpSuccess,
+    UnexpectedHttpCode,
+    /// Response did not contain a field that we expected
+    #[error(display = "Response did not contain an expected field.")]
+    PoorlyFormedResponse,
     #[error(
         display = "A transport protocol message could not be (de)serialized: {}.",
         _0
@@ -75,16 +76,52 @@ pub enum HttpResponse {
     PartialContent(Vec<u8>), // 206: Body
 }
 
+fn convert_reqwest_response_to_http_response(res: reqwest::blocking::Response) -> Result<HttpResponse, HttpError> {
+    let response = match res.status() {
+        reqwest::StatusCode::OK => {
+            let body = res.bytes().map_err(|_| HttpError::PoorlyFormedResponse)?;
+            HttpResponse::Ok(body.to_vec())
+        },
+        reqwest::StatusCode::CREATED => {
+            let location = match res.headers().get(reqwest::header::LOCATION) {
+                None => return Err(HttpError::PoorlyFormedResponse),
+                Some(loc) => loc.to_str().map_err(|_| HttpError::PoorlyFormedResponse)?.to_string(),
+            };
+            let body = res.bytes().map_err(|_| HttpError::PoorlyFormedResponse)?;
+
+            HttpResponse::Created(location, body.to_vec())
+        }
+        reqwest::StatusCode::ACCEPTED => {
+            let body = res.bytes().map_err(|_| HttpError::PoorlyFormedResponse)?;
+            HttpResponse::Accepted(body.to_vec())
+        }
+        reqwest::StatusCode::NON_AUTHORITATIVE_INFORMATION => {
+            let body = res.bytes().map_err(|_| HttpError::PoorlyFormedResponse)?;
+            HttpResponse::NonAuthoritativeInformation(body.to_vec())
+        }
+        reqwest::StatusCode::NO_CONTENT => HttpResponse::NoContent,
+        reqwest::StatusCode::RESET_CONTENT => {
+            let body = res.bytes().map_err(|_| HttpError::PoorlyFormedResponse)?;
+            HttpResponse::ResetContent(body.to_vec())
+        }
+        reqwest::StatusCode::PARTIAL_CONTENT => {
+            let body = res.bytes().map_err(|_| HttpError::PoorlyFormedResponse)?;
+            HttpResponse::PartialContent(body.to_vec())
+        }
+        _ => return Err(HttpError::UnexpectedHttpCode),
+    };
+    return Ok(response);
+}
+
 /// Sends an encoded `buffer` via HTTP to a server at `url`.  Fails if the
 /// Curl session fails for any reason, or if a non-success HTTP code is
 /// returned.
-pub fn post_string<U, B>(url: U, buffer: B, content_type_option: Option<&str>) -> AnyhowResult<HttpResponse, HttpError>
+pub fn post_string<U>(url: U, buffer: &String, content_type_option: Option<&str>) -> Result<HttpResponse, HttpError>
 where
     U: AsRef<str>,
-    B: AsRef<str>,
 {
     let url = url.as_ref();
-    let buffer = buffer.as_ref();
+    let buffer: String = buffer.to_string();
 
     info!(
         "Posting buffer {} ({} bytes) to {}.",
@@ -93,327 +130,54 @@ where
         url
     );
 
-    let mut curl_request = Easy::new();
-
-    curl_request.url(url).map_err(|err| {
-        error!("Failed to set URL with Curl.  Error produced: {:?}.", err);
-
-        return HttpError::CurlError(err);
-    })?;
-
-    let mut headers = List::new();
-    if let Some(content_type) = content_type_option {
-        headers
-            .append(format!("Content-Type: {:}", content_type).as_str())
-            .map_err(|err| {
-                error!(
-                    "Failed to append `Content-Type` {:}. Error produced: {:?}",
-                    content_type,
-                    err
-                );
-
-                HttpError::CurlError(err)
-            })?;
-    } else {
-        headers
-            .append("Content-Type: application/octet-stream")
-            .map_err(|err| {
-                error!(
-                    "Failed to append `Content-Type` header.  Error produced: {:?}.",
-                    err
-                );
-
-                HttpError::CurlError(err)
-            })?;
-    }
-
-    curl_request.http_headers(headers).map_err(|err| {
-        error!(
-            "Failed to set HTTP headers with Curl.  Error produced: {:?}.",
-            err
-        );
-
-        return HttpError::CurlError(err);
-    })?;
-    curl_request.post(true).map_err(|err| {
-        error!(
-            "Failed to set post field to `true` with Curl.  Error produced: {:?}.",
-            err
-        );
-
-        return HttpError::CurlError(err);
-    })?;
-    curl_request
-        .post_field_size(buffer.len() as u64)
-        .map_err(|err| {
-            error!(
-                "Failed to set post field size with Curl.  Error produced: {:?}.",
-                err
-            );
-
-            return HttpError::CurlError(err);
-        })?;
-
-    let mut buffer_reader = StringReader::new(buffer);
-    let mut received_body: Vec<u8> = Vec::new();
-    let mut received_header = String::new();
-
-    {
-        let mut transfer = curl_request.transfer();
-
-        transfer
-            .read_function(|buf| Ok(buffer_reader.read(buf).unwrap_or(0)))
-            .map_err(|err| {
-                error!(
-                    "Failed to register read function with Curl.  Error produced: {:?}.",
-                    err
-                );
-
-                return HttpError::CurlError(err);
-            })?;
-
-        transfer
-            .write_function(|buf| {
-                received_body.extend_from_slice(buf);
-
-                Ok(buf.len())
-            })
-            .map_err(|err| {
-                error!(
-                    "Failed to register write function with Curl.  Error produced: {:?}.",
-                    err
-                );
-
-                return HttpError::CurlError(err);
-            })?;
-
-        info!("Received response body.");
-
-        transfer
-            .header_function(|buf| {
-                received_header.push_str(from_utf8(buf).unwrap_or_else(|_| {
-                    panic!("{}", {
-                        trace!(
-                            "Error converting data {:?} from UTF-8.  Continuing with default value.",
-                            buf
-                        );
-
-                        &format!("Error converting data {:?} from UTF-8", buf)
-                    })
-                }));
-
-                true
-            })
-            .map_err(|err| {
-                error!(
-                    "Failed to register header function with Curl.  Error produced: {:?}.",
-                    err
-                );
-
-                return HttpError::CurlError(err);
-            })?;
-
-        transfer.perform().map_err(|err| {
-            error!(
-                "Failed to perform data transfer with Curl.  Error produced: {:?}.",
-                err
-            );
-
-            return HttpError::CurlError(err);
-        })?;
-    }
-
-    info!("Received response header: {}.", received_header);
-    let response = if received_header.contains("HTTP/1.1 200 OK\r") {
-        HttpResponse::Ok(received_body)
-    } else if received_header.contains("HTTP/1.1 201 Created\r") {
-        // Get the "Location" field from the header
-        let location_regex = Regex::new(r"Location:\s(.*)\r").unwrap();
-        let location = location_regex.captures(&received_header).unwrap().get(1).unwrap().as_str();
-        HttpResponse::Created(location.to_string(), received_body)
-    } else if received_header.contains("HTTP/1.1 202 Accepted\r") {
-        HttpResponse::Accepted(received_body)
-    } else if received_header.contains("HTTP/1.1 203 Non-Authoritative Information\r") {
-        HttpResponse::NonAuthoritativeInformation(received_body)
-    } else if received_header.contains("HTTP/1.1 204 No Content\r") {
-        HttpResponse::NoContent
-    } else if received_header.contains("HTTP/1.1 205 Reset Content\r") {
-        HttpResponse::ResetContent(received_body)
-    } else if received_header.contains("HTTP/1.1 206 Partial Content\r") {
-        HttpResponse::PartialContent(received_body)
-    } else {
-        println!("post_string: received_header:{:?}", received_header);
-        return Err(HttpError::HttpSuccess);
+    let request_builder = reqwest::blocking::Client::new()
+        .post(url)
+        .body(buffer);
+    
+    let request_builder = match content_type_option {
+        Some(content_type) => {
+            request_builder.header(reqwest::header::CONTENT_TYPE, reqwest::header::HeaderValue::from_str(content_type).map_err(|err| HttpError::InvalidHeaderValue(err))?)
+        }
+        None => request_builder, // do nothing
     };
-    Ok(response)
+    let ret = request_builder.send()
+        .map_err(|err| HttpError::ReqwestError(err))?;
+    let response = convert_reqwest_response_to_http_response(ret)?;
+
+    return Ok(response);
 }
 
-/// Sends an encoded `buffer` via HTTP to a server at `url`.  Fails if the
+/// Sends an raw byte `buffer` via HTTP to a server at `url`.  Fails if the
 /// Curl session fails for any reason, or if a non-success HTTP code is
 /// returned.
-pub fn post_bytes<U, B>(url: U, buffer: B, content_type_option: Option<&str>) -> AnyhowResult<HttpResponse, HttpError>
+pub fn post_bytes<U>(url: U, buffer: &[u8], content_type_option: Option<&str>) -> Result<HttpResponse, HttpError>
 where
     U: AsRef<str>,
-    B: AsRef<[u8]>,
 {
     let url = url.as_ref();
-    let buffer = buffer.as_ref();
+    let buffer = buffer.to_vec();
 
-    // info!(
-    //     "Posting buffer {} ({} bytes) to {}.",
-    //     buffer,
-    //     buffer.len(),
-    //     url
-    // );
+    info!(
+        "Posting byte buffer {:?} ({} bytes) to {}.",
+        buffer,
+        buffer.len(),
+        url
+    );
 
-    let mut curl_request = Easy::new();
-
-    curl_request.url(url).map_err(|err| {
-        error!("Failed to set URL with Curl.  Error produced: {:?}.", err);
-        HttpError::CurlError(err)
-    })?;
-
-    let mut headers = List::new();
-    if let Some(content_type) = content_type_option {
-        headers
-            .append(format!("Content-Type: {:}", content_type).as_str())
-            .map_err(|err| {
-                error!(
-                    "Failed to append `Content-Type` {:}. Error produced: {:?}",
-                    content_type,
-                    err
-                );
-                HttpError::CurlError(err)
-            })?;
-    } else {
-        headers
-            .append("Content-Type: application/octet-stream")
-            .map_err(|err| {
-                error!(
-                    "Failed to append `Content-Type` header.  Error produced: {:?}.",
-                    err
-                );
-
-                HttpError::CurlError(err)
-            })?;
-    }
-
-    curl_request.http_headers(headers).map_err(|err| {
-        error!(
-            "Failed to set HTTP headers with Curl.  Error produced: {:?}.",
-            err
-        );
-
-        HttpError::CurlError(err)
-    })?;
-    curl_request.post(true).map_err(|err| {
-        error!(
-            "Failed to set post field to `true` with Curl.  Error produced: {:?}.",
-            err
-        );
-
-        HttpError::CurlError(err)
-    })?;
-    curl_request
-        .post_field_size(buffer.len() as u64)
-        .map_err(|err| {
-            error!(
-                "Failed to set post field size with Curl.  Error produced: {:?}.",
-                err
-            );
-            HttpError::CurlError(err)
-        })?;
-
-    let mut buffer_reader = std::io::Cursor::new(buffer);
-    let mut received_body: Vec<u8> = Vec::new();
-    let mut received_header = String::new();
-    {
-        let mut transfer = curl_request.transfer();
-
-        transfer
-            .read_function(|buf| Ok(buffer_reader.read(buf).unwrap_or(0)))
-            .map_err(|err| {
-                error!(
-                    "Failed to register read function with Curl.  Error produced: {:?}.",
-                    err
-                );
-
-                HttpError::CurlError(err)
-            })?;
-
-        transfer
-            .write_function(|buf| {
-                received_body.extend_from_slice(buf);
-
-                Ok(buf.len())
-            })
-            .map_err(|err| {
-                error!(
-                    "Failed to register write function with Curl.  Error produced: {:?}.",
-                    err
-                );
-
-                HttpError::CurlError(err)
-            })?;
-
-        info!("Received response body.");
-
-        transfer
-            .header_function(|buf| {
-                received_header.push_str(from_utf8(buf).unwrap_or_else(|_| {
-                    panic!("{}", {
-                        trace!(
-                            "Error converting data {:?} from UTF-8.  Continuing with default value.",
-                            buf
-                        );
-
-                        &format!("Error converting data {:?} from UTF-8", buf)
-                    })
-                }));
-
-                true
-            })
-            .map_err(|err| {
-                error!(
-                    "Failed to register header function with Curl.  Error produced: {:?}.",
-                    err
-                );
-
-                HttpError::CurlError(err)
-            })?;
-
-        transfer.perform().map_err(|err| {
-            error!(
-                "Failed to perform data transfer with Curl.  Error produced: {:?}.",
-                err
-            );
-
-            HttpError::CurlError(err)
-        })?;
-    }
-
-    let response = if received_header.contains("HTTP/1.1 200 OK\r") {
-        HttpResponse::Ok(received_body)
-    } else if received_header.contains("HTTP/1.1 201 Created\r") {
-        // Get the "Location" field from the header
-        let location_regex = Regex::new(r"Location:\s(.*)\r").unwrap();
-        let location = location_regex.captures(&received_header).unwrap().get(1).unwrap().as_str();
-        HttpResponse::Created(location.to_string(), received_body)
-    } else if received_header.contains("HTTP/1.1 202 Accepted\r") {
-        HttpResponse::Accepted(received_body)
-    } else if received_header.contains("HTTP/1.1 203 Non-Authoritative Information\r") {
-        HttpResponse::NonAuthoritativeInformation(received_body)
-    } else if received_header.contains("HTTP/1.1 204 No Content\r") {
-        HttpResponse::NoContent
-    } else if received_header.contains("HTTP/1.1 205 Reset Content\r") {
-        HttpResponse::ResetContent(received_body)
-    } else if received_header.contains("HTTP/1.1 206 Partial Content\r") {
-        HttpResponse::PartialContent(received_body)
-    } else {
-        println!("post_bytes: received_header:{:?}", received_header);
-        return Err(HttpError::HttpSuccess);
+    let request_builder = reqwest::blocking::Client::new()
+        .post(url)
+        .body(buffer);
+    let request_builder = match content_type_option {
+        Some(content_type) => {
+            request_builder.header(reqwest::header::CONTENT_TYPE, reqwest::header::HeaderValue::from_str(content_type).map_err(|err| HttpError::InvalidHeaderValue(err))?)
+        }
+        None => request_builder, // do nothing
     };
-    Ok(response)
+    let ret = request_builder.send()
+        .map_err(|err| HttpError::ReqwestError(err))?;
+    let response = convert_reqwest_response_to_http_response(ret)?;
+
+    return Ok(response);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -432,10 +196,10 @@ pub fn send_proxy_attestation_server_start<U: AsRef<str>>(
     info!("Sending Start message to Proxy Attestation Service.");
 
 
-    let url = format!("{}/proxy/v1/Start", proxy_attestation_server_url_base);
+    let url = format!("http://{}/proxy/v1/Start", proxy_attestation_server_url_base);
     let empty_buffer: Vec<u8> = Vec::new();
 
-    let (id, nonce) = match post_bytes(&url, empty_buffer, None).map_err(|e| {
+    let (id, nonce) = match post_bytes(&url, &empty_buffer, None).map_err(|e| {
             error!(
                 "Failed to send proxy attestation service start message.  Error produced: {}.",
                 e
@@ -447,7 +211,7 @@ pub fn send_proxy_attestation_server_start<U: AsRef<str>>(
         }
         non_created => {
             println!("Received incorrect response:{:?} from post_string", non_created);
-            return Err(anyhow!(HttpError::HttpSuccess));
+            return Err(anyhow!(HttpError::UnexpectedHttpCode));
         }
     };
     println!("calling parse_str on id:{:?}", id);
