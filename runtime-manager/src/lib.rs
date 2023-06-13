313172ad3179027136bb6505607381dc3a6604c3
Mathias Brossard <mathias.brossard@arm.com>
2023-06-13 10:19:19 -0500

    Make new crates consistent and align dependencies
    Fix version and edition on new crates
    Soft-pinning rustix to v0.35 and tempfile to v3.3

--- runtime-manager/src/lib.rs
+++ runtime-manager/src/lib.rs
@@ -9,10 +9,6 @@
 //! See the `LICENSE_MIT.markdown` file in the Veracruz root directory for
 //! information on licensing and copyright.
 
-#![cfg_attr(any(feature = "icecap"), no_main)]
-#![cfg_attr(any(feature = "icecap"), feature(rustc_private))]
-#![cfg_attr(any(feature = "icecap"), feature(format_args_nl))]
-
 pub mod common_runtime;
 pub mod managers;
 pub mod platforms;
